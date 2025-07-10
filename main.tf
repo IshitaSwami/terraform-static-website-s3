c08ae9ef2ada75eded62cade4a9b03737434e721
Ishita Swami <ishita.swami.1998@gmail.com>
2025-07-11 00:48:12 +0530

    Day 4 - Upload static website to S3 using Terraform

---
+++ main.tf
@@ -0,0 +1,63 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "static_site" {
+  bucket = var.bucket_name
+
+  tags = local.common_tags
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_public" {
+  bucket = aws_s3_bucket.static_site.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.static_site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "public_policy" {
+  bucket = aws_s3_bucket.static_site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.static_site.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.static_site.id
+  key          = "index.html"
+  source       = "site/index.html"
+  content_type = "text/html"
+  acl          = "public-read"
+}
+
+resource "aws_s3_object" "error" {
+  bucket       = aws_s3_bucket.static_site.id
+  key          = "error.html"
+  source       = "site/error.html"
+  content_type = "text/html"
+  acl          = "public-read"
+}
---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  description = "AWS Region to deploy"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  default = "static-site-demo-20250702-aj"
+}
+
